522de270fa8ad000f9443c4e8c785d3ee39f17d8
KennedyUC <chukwukakennedyuche@gmail.com>
2022-01-27 07:57:45 -0800

    initial commit

---
+++ example/test/variables.tf
@@ -0,0 +1,20 @@
+variable "resource_group_name" { 
+}
+
+variable "location" { 
+}
+
+variable "acr_name" { 
+}
+
+variable "subscription_id" { 
+}
+
+variable "tenant_id" { 
+}
+
+variable "client_id" { 
+}
+
+variable "client_secret" { 
+}
