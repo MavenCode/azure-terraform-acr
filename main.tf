388d7a0ecdb7a227b2749ba4db85cb91e59c4a7b
KennedyUC <chukwukakennedyuche@gmail.com>
2022-02-01 03:40:44 -0800

    terraform files modified

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "azurerm_container_registry" "acr" {
     resource_group_name = azurerm_resource_group.rg.name
     location            = azurerm_resource_group.rg.location
 
-    sku                 = "Premium"
+    sku                 = var.sku
 
-    admin_enabled       = true
+    admin_enabled       = var.admin_enabled
 }
