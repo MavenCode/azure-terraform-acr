8163ec3df0e087bbf21a9e930730637724ea2edb
KennedyUC <chukwukakennedyuche@gmail.com>
2022-02-04 11:03:07 -0800

    file modified

--- variables.tf
+++ variables.tf
@@ -1,12 +1,2 @@
 variable "acr_name" { 
 }
-
-variable "admin_enabled" {
-    description = "Enable admin control. Takes True or False as input"
-    default     = "True"
-}
-
-variable "sku" {
-    description = "SKU (multiple service tier) name of container registry. Possible values: Basic, Standard and Premium"
-    default  = "Premium"
-}
