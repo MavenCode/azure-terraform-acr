892fac4d06bdbd5461221a18bfba50e420eaf7c7
Kennedy Uche <94866393+kennedyuche@users.noreply.github.com>
2022-02-11 17:10:44 -0800

    resource group resource removed from module

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.resource_group_location
-}
+# resource "azurerm_resource_group" "rg" {
+#   name     = var.resource_group_name
+#   location = var.resource_group_location
+# }
 
 resource "azurerm_container_registry" "acr" {
     name                = var.acr_name
-    resource_group_name = azurerm_resource_group.rg.name
-    location            = azurerm_resource_group.rg.location
+    resource_group_name = var.resource_group_name
+    location            = var.resource_group_location
 
     sku                 = "Premium"
 
